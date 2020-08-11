7296c9b47f09d025407914343b1888ff653d3e5d
eduardo bisinella <bisinella7@gmail.com>
2020-08-11 13:40:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Project-Next-Level
+
+Aqui está um projeto de uma plataforma feita para conectar alunos e professores seu nome se chama Proffy, desenvolvida durante a Next Level Week 2 da Rocketseat.
+
+# Tecnologias 
+
+JavaScript<br>
+Node<br>
+Express<br>
+SQLite3
+
---
+++ src/database/fake_data.js
@@ -0,0 +1,26 @@
+const proffys = [
+  {
+    name: "Diego Fernandes",
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+    whatsapp: "899965262651",
+    bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Química",
+    cost: "R$ 20",
+    weekday: [0],
+    time_from: [720],
+    time_to: [1220],
+
+  },
+  {
+    name: "Eduardo Bisinella",
+    avatar: "https://avatars1.githubusercontent.com/u/64981838?s=460&u=bf3d883fd0b2667e1a96bc90b97a74456bf216ed&v=4",
+    whatsapp: "899965262651",
+    bio: "Let's Go.<br><br> Apaixonado por futebol e louco por codar",
+    subject: "Educação fisica",
+    cost: "R$ 20",
+    weekday: [1],
+    time_from: [720],
+    time_to: [1220],
+
+  }
+]
